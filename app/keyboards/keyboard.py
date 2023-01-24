65199825415afbf4169e1ee5aa3c456dee2ab071
pampam0 <m.shilov@code-construction.ru>
2023-01-24 18:54:47 +0700

    deleted start button

--- app/keyboards/keyboard.py
+++ app/keyboards/keyboard.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-start = KeyboardButton('/start')
+
 load = KeyboardButton('Загрузить')
 get_all = KeyboardButton('Список рецептов')
 category = KeyboardButton('Рецепты по категориям')
@@ -10,11 +10,11 @@ delete = KeyboardButton('Удалить рецепт')
 cancel = KeyboardButton('Отмена')
 
 admin_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-admin_keyboard.row(start, get_all, category).row(load, update, delete)
+admin_keyboard.row(get_all, category).row(load, update, delete)
 
 client_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-client_keyboard.add(start, get_all, category)
+client_keyboard.add(get_all, category)
 
 cancel_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-cancel_keyboard.add(cancel, start)
+cancel_keyboard.add(cancel)
 
